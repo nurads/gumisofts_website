36633be39aaddd57ee02942a0098ebe2c788c542
kaleb wondimu <kalebwondimu33@gmail.com>
2025-03-20 16:23:03 +0300

    working on carrer page

--- app/careers/page.tsx
+++ app/careers/page.tsx
@@ -1,7 +1,18 @@
 import React from "react";
-
-const page = () => {
-  return <div></div>;
+import Image from "next/image";
+const careers = () => {
+  return (
+    <div className="relative w-full max-w-2xl h-64   mx-auto mt-24">
+      <Image
+        src="/assets/careerHomepageImage.png"
+        alt="Card Image"
+        layout="fill"
+      />
+      <div className="absolute bottom-4 right-4 bg-black bg-opacity-50 text-white px-3 py-1 rounded-md text-sm">
+        WITH GUMISOFTS
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default careers;
